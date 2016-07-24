c948664c75f6f0568f43264a6941a42a4b0256a0
Guillaume BITAUD <guillaume.bitaud@gmail.com>
2016-07-24 09:56:21 +0100

    Change name of stm32f4xxit.x by event.c and .h

---
+++ Coupe_2017/Inc/event.h
@@ -0,0 +1,28 @@
+/**
+  ******************************************************************************
+  * @file    stm32f4xx_it.h
+  * @brief   This file contains the headers of the interrupt handlers.
+  ******************************************************************************/
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __STM32F4xx_IT_H
+#define __STM32F4xx_IT_H
+
+	#include "stm32f4xx_hal.h"
+	#include "stm32f4xx.h"
+
+	/** SysTick Handler*/
+	void SysTick_Handler(void);
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __STM32F4xx_IT_H */
+
+/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
